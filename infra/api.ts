d42071c06db0cb6f6bca569c1a72a6d6b5f1386f
Ross <ross.acheson@gmail.com>
2024-10-23 11:33:48 -0600

    Finish adding Notes CRUD

--- infra/api.ts
+++ infra/api.ts
@@ -10,4 +10,8 @@ export const api = new sst.aws.ApiGatewayV2("Api", {
   }
 });
 
-api.route("POST /notes", "packages/functions/src/create.main");
+api.route("POST /notes", "packages/functions/src/notes/create.main");
+api.route("GET /notes/{id}", "packages/functions/src/notes/get.main");
+api.route("GET /notes", "packages/functions/src/notes/list.main");
+api.route("PUT /notes/{id}", "packages/functions/src/notes/update.main");
+api.route("DELETE /notes/{id}", "packages/functions/src/notes/delete.main");
