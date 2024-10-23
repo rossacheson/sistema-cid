5c327396a3cbcb27ab80fe1a58826141155c5921
Ross <ross.acheson@gmail.com>
2024-10-23 10:19:18 -0600

    Add Notes table

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,2 +1,10 @@
 // Create an S3 bucket
 export const bucket = new sst.aws.Bucket("Uploads");
+
+export const notesTable = new sst.aws.Dynamo("Notes", {
+    fields: {
+        userId: "string",
+        noteId: "string",
+    },
+    primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
+});
