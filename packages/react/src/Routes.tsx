bf735174e1a74fc593fbb92da84dd6499a65de5a
Ross <ross.acheson@gmail.com>
2024-10-25 11:52:45 -0600

    Add page to view/update a note

--- packages/react/src/Routes.tsx
+++ packages/react/src/Routes.tsx
@@ -4,17 +4,19 @@ import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
 import NewNote from './containers/NewNote.tsx';
+import Notes from './containers/Notes.tsx';
 
 export default function Links() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/notes/new" element={<NewNote />} />
-      
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/notes/new" element={<NewNote/>}/>
+      <Route path="/notes/:id" element={<Notes/>}/>
+
       {/* Finally, catch all unmatched routes */}
-      <Route path="*" element={<NotFound />} />;
+      <Route path="*" element={<NotFound/>}/>;
     </Routes>
   );
 }
