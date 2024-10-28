8fa7de7a142ceb97f74c1c7cf1ede3f7e730b482
Ross <ross.acheson@gmail.com>
2024-10-28 10:15:01 -0600

    Some cleanup

--- packages/react/src/containers/Settings.tsx
+++ packages/react/src/containers/Settings.tsx
@@ -1,9 +1,3 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 export default function Settings() {
-  const nav = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
-
   return <div className="Settings"></div>;
 }
