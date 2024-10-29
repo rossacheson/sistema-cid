9a2c12982cfbd10d9362e312233d3af68efc9415
Ross <ross.acheson@gmail.com>
2024-10-28 23:52:18 -0600

    Add Angular Material and start to set up navigation

--- packages/angular/src/app/app.routes.ts
+++ packages/angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BienvenidoComponent } from './pages/bienvenido/bienvenido.component';
+import { PersonasComponent } from './pages/personas/personas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'personas', component: PersonasComponent },
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: '', redirectTo: 'bienvenido', pathMatch: 'full' },
+];
---
+++ packages/angular/src/app/pages/personas/personas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personas',
+  standalone: true,
+  imports: [],
+  templateUrl: './personas.component.html',
+  styleUrl: './personas.component.scss'
+})
+export class PersonasComponent {
+
+}
